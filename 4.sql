b5e454f2ddcfe8e3a4e1b550c884053589025d57
dmsoldatov <dm.soldatov@yandex.ru>
2021-02-26 18:17:22 +0300

    Update lost_operation alg

--- 4.sql
+++ 4.sql
@@ -30,35 +30,35 @@ select
 	case
 		when scenario in ('inObj_noExitOpers', 'inObj_lastOper-Enter', 'inObj_ExitSameBorder') then f_msk_dts
 		when scenario in ('inWay_noValidExit', 'inWay_ValidExit') then operWithIndexInWay_msk_dts
-		when scenario = 'LostRpo' then Null
+		when scenario = 'LostRpo' then LostOperation_msk_dts
 		when scenario = 'unknown' then LastExNowhOp_msk_dts 
 		else null
 	end as status_change_msk_dts,
 	case
 		when scenario in ('inObj_noExitOpers', 'inObj_lastOper-Enter', 'inObj_ExitSameBorder') then f_local_dts
 		when scenario in ('inWay_noValidExit', 'inWay_ValidExit') then operWithIndexInWay_local_dts
-		when scenario = 'LostRpo' then Null
+		when scenario = 'LostRpo' then LostOperation_local_dts
 		when scenario = 'unknown' then LastExNowhOp_local_dts 
 		else null
 	end as status_change_local_dts,
 	case
 		when scenario in ('inObj_noExitOpers', 'inObj_lastOper-Enter', 'inObj_ExitSameBorder') then f_local_timezone
 		when scenario in ('inWay_noValidExit', 'inWay_ValidExit') then operWithIndexInWay_local_timezone
-		when scenario = 'LostRpo' then Null
+		when scenario = 'LostRpo' then LostOperation_timezone
 		when scenario = 'unknown' then LastExNowhOp_local_timezone 
 		else null
 	end as status_change_timezone,
 	case 
 		when scenario in ('inObj_noExitOpers', 'inObj_lastOper-Enter', 'inObj_ExitSameBorder') then f_objectborder_index_ccode
 		when scenario in ('inWay_noValidExit', 'inWay_ValidExit') then operWithIndexInWay_border_index
-		when scenario = 'LostRpo' then lostrpo_border_index
+		when scenario = 'LostRpo' then LostOperation_border_index
 		when scenario = 'unknown' then Null 
 		else null
 	end as opsindex_ccode, 
 	case
 		when scenario in ('inObj_noExitOpers', 'inObj_lastOper-Enter', 'inObj_ExitSameBorder') then f_country
 		when scenario in ('inWay_noValidExit', 'inWay_ValidExit') then operWithIndexInWay_country
-		when scenario = 'LostRpo' then lostrpo_country
+		when scenario = 'LostRpo' then LostOperation_country
 		when scenario = 'unknown' then Null 
 		else null
 	end as country_ccode,
@@ -147,27 +147,30 @@ select
 		then first_value(local_timezone) over (partition by rpobarcode_ccode order by isExitNowhere_flg DESC, msk_dts DESC)
 		else null
 	end as LastExNowhOp_local_timezone,
+-- LostOperation
 	case 
 		when max_lostOperation_flg = 1
 		then first_value(msk_dts) over (partition by rpobarcode_ccode order by lostOperation_flg DESC, msk_dts DESC)
 		else null
 	end as LostOperation_msk_dts,
 	case 
-		when max_lastGroupedOper_rank > 0
-		then first_value(country_ncode) over (partition by rpobarcode_ccode order by lastGroupedOper_rank DESC)
-		when max_operWithIndexInWay_rank > 0
-		then first_value(country_ncode) over (partition by rpobarcode_ccode order by operWithIndexInWay_rank DESC, msk_dts DESC)
+		when max_lostOperation_flg = 1
+		then first_value(local_dts) over (partition by rpobarcode_ccode order by lostOperation_flg DESC, msk_dts DESC)
 		else null
-	end as lostrpo_country,
+	end as LostOperation_msk_dts,
 	case 
-		when max_lastGroupedOper_rank > 1
-		then first_value(objectborder_index_ccode) over (partition by rpobarcode_ccode order by lastGroupedOper_rank DESC)
-		when max_lastGroupedOper_rank = 1 and indexnext_border is not null
-		then first_value(indexnext_border) over (partition by rpobarcode_ccode order by lastGroupedOper_rank DESC)
-		when max_operWithIndexInWay_rank > 0
-		then first_value(next_border) over (partition by rpobarcode_ccode order by operWithIndexInWay_rank DESC, msk_dts DESC)
-		else NULL
-	end as lostrpo_border_index,
+		when max_lostOperation_flg = 1
+		then first_value(local_timezone) over (partition by rpobarcode_ccode order by lostOperation_flg DESC, msk_dts DESC)
+		else null
+	end as LostOperation_timezone,
+	case 
+		when max_lostOperation_flg = 1
+		then first_value(objectborder_index_ccode) over (partition by rpobarcode_ccode order by lostOperation_flg DESC, msk_dts DESC)
+	end as LostOperation_border_index,
+	case 
+		when max_lostOperation_flg = 1
+		then first_value(country_ncode) over (partition by rpobarcode_ccode order by lostOperation_flg DESC, msk_dts DESC)
+	end as LostOperation_country,
 	isValidExit_msk_dts,
 	last_oper_date
 from (
@@ -183,7 +186,6 @@ select *,
 	first_value(objectborder_index_ccode) over (partition by rpobarcode_ccode order by isValidExit_flg DESC, msk_dts DESC) as isValidExit_objectborder,
 	first_value(next_index) over (partition by rpobarcode_ccode order by isValidExit_flg DESC, msk_dts DESC) as isValidExit_next_index,
 	max(operWithIndexInWay_rank) over (partition by rpobarcode_ccode) as max_operWithIndexInWay_rank,
-	max(lastGroupedOper_rank) over (partition by rpobarcode_ccode) as max_lastGroupedOper_rank
 from ${dwh4dm_scm}.${trnst_60days_opers_step3}
 ) firstlvl
 ) secondlvl
